edeb561f614270b5bac2ebec1c74046dc43936ba
tenmark86e <tenmark86@gmail.com>
2022-08-03 10:23:34 +0300

    completed looping lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+const event = "surprise";
+
+ 
+ function writeCards( names, event ) {
+  let personalCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    personalCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return personalCards
+}
+
+function countDown(){
+    let numCountdown=11;
+    while(numCountdown--){
+      console.log(numCountdown)
+    }
+  } 
